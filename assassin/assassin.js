d0a3d055c2999388fa1f4ed85d3687c4ba8c9f33
Sneha Silwal <ss4547@columbia.edu>
2014-10-18 16:24:36 -0400

    testing google example

--- assassin/assassin.js
+++ assassin/assassin.js
@@ -1,5 +1,6 @@
+var map;
 if (Meteor.isClient) {
-    
+  
     
   // counter starts at 0
   Session.setDefault("counter", 0);
@@ -13,9 +14,6 @@ if (Meteor.isClient) {
 //             return "Hi";
 //         }
     }
-
-
-    }
   });
 
   Template.hello.events({
@@ -24,9 +22,59 @@ if (Meteor.isClient) {
       Session.set("counter", Session.get("counter") + 1);
     }
   });
-   
+
 }
 
+  Template.startup({
+      initialize: function(){
+        var mapOptions = {
+        zoom: 6
+      };
+
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+       // Try HTML5 geolocation
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = new google.maps.LatLng(position.coords.latitude,
+                                           position.coords.longitude);
+
+          var infowindow = new google.maps.InfoWindow({
+            map: map,
+            position: pos,
+            content: 'Location found using HTML5.'
+          });
+
+          map.setCenter(pos);
+        }, function() {
+          handleNoGeolocation(true);
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+      }
+    }
+
+    function handleNoGeolocation(errorFlag) {
+      if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+      } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+      }
+
+      var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+      };
+
+      var infowindow = new google.maps.InfoWindow(options);
+      map.setCenter(options.position);
+    }
+
+  });
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
