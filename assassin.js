4b0b0dc002e362ea883cf5a645328816ea9dd96b
Albert Cui <aqc2109@columbia.edu>
2014-10-18 23:30:33 -0400

    try to add other users

--- assassin.js
+++ assassin.js
@@ -5,7 +5,8 @@ var marker;
 function getLocation() {
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
-       Players.update(Players.findOne({name: "test"})._id, {$set: {lat: position.coords.latitude, lon: position.coords.longitude}})
+      if (Meteor.user())
+       Players.update(Meteor.userid, {$set: {lat: position.coords.latitude, lon: position.coords.longitude}})
        marker.setPosition(
            new google.maps.LatLng(
                position.coords.latitude,
@@ -16,7 +17,8 @@ function getLocation() {
 }
 
 if (Meteor.isClient) {
-
+  Meteor.subscribe("userData");
+  Meteor.subscribe("players");
 
   Template.map.rendered = function() {
     var layer = "toner";
@@ -42,21 +44,70 @@ if (Meteor.isClient) {
 
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(function(position){
-         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         map.setCenter(initialLocation);
         map.setZoom(18);
         marker.setPosition(initialLocation);
       });
-
     }
 
     Meteor.setInterval(getLocation, 1000)
+    var markers;
+    if (!this.handle) {
+      this.handle = Deps.autorun(function () {
+        console.log("called")
+        Players.find({}).forEach(function(p){
+          if(markers[p.username] && p.lat) {
+            markers[p.username][marker].setPosition(p.lat, p.long)
+            markers[p.username][updated] = true;
+          } else if (p.lat) {
+            markers[p.username][updated] = true;
+            markers[p.username][marker] = new google.maps.Marker({
+              position: new google.maps.LatLng(p.lat, p.long),
+              map: map,
+              title: p.username
+            })
+          }
+
+          for (var property in markers) {
+            if (markers.hasOwnProperty(property)) {
+              if (markers[property][updated]) {
+                markers[property][updated] = false;
+              } else {
+                markers[property][marker].setMap(null)
+                delete markers[property]
+              }
+            }
+          }
+        })
+      })
+    }
   }
+
+    Template.map.destroyed = function () {
+      this.handle && this.handle.stop();
+    };
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0)
       Players.insert({name: "test"});
+  });
+
+  Meteor.publish("userData", function () {
+    if (this.userId) {
+      return Meteor.users.find(
+        {_id: this.userId},
+        {fields: {lat: 1, lon: 1}}
+      );
+    } else {
+      this.ready();
+    }
+  });
+
+  Meteor.publish("players", function () {
+    return Meteor.users.find({"status.online": true}, {fields: {lat: 1, lon: 1}});
   });
 }
