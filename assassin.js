3f6624918abaa0d35e8bbcdfb6ab902aff712eff
Albert Cui <aqc2109@columbia.edu>
2014-10-18 20:46:11 -0400

    continuously update position

--- assassin.js
+++ assassin.js
@@ -1,19 +1,56 @@
 Players = new Mongo.Collection("players");
 
+var marker;
+
 function getLocation() {
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
-       Players.update(Session.get("player")._id, {$set: {lat: position.coords.latitude, lon: position.coords.longitude}})
+       Players.update(Players.findOne({name: "test"})._id, {$set: {lat: position.coords.latitude, lon: position.coords.longitude}})
+       marker.setPosition(
+           new google.maps.LatLng(
+               position.coords.latitude,
+               position.coords.longitude)
+       );
     });
   }
 }
 
 if (Meteor.isClient) {
-  while(!Session.get("player")) {
-      Session.set("player", Players.findOne({name: "test"}))
-  }
 
-  Meteor.setInterval(getLocation, 1000)
+  Template.map.rendered = function() {
+    var layer = "toner";
+    var myLatlng1 = new google.maps.LatLng(53.65914, 0.072050);
+    var mapOptions = {
+      zoom: 8,
+      mapTypeId: layer,
+      mapTypeControlOptions: {
+        mapTypeIds: [layer]
+      }
+    }
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    map.mapTypes.set(layer, new google.maps.StamenMapType(layer));
+
+    marker = new google.maps.Marker({
+        position: myLatlng1,
+        map: map,
+        title: 'Hello World!'
+      });
+
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+        map.setZoom(18);
+        marker.setPosition(initialLocation);
+      });
+
+    }
+
+    Meteor.setInterval(getLocation, 1000)
+  }
 }
 
 if (Meteor.isServer) {
